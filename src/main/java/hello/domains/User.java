29057cd64a11bafe010e8f08e7cba0e7541b081e
AnatolyMoiseev <moiseev199796@gmail.com>
2018-12-18 12:36:17 +0300

    Fixes

--- src/main/java/hello/domains/User.java
+++ src/main/java/hello/domains/User.java
@@ -15,7 +15,6 @@ public class User {
     @Id
     @GenericGenerator(name = "gen", strategy = "increment")
     @GeneratedValue(generator = "gen")
-    //@GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String name;
     private String email;
